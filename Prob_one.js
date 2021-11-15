8e524bfffbb2e060ae180407c6f4e92abc8ced3c
abhiramkrishna.8921@gmail.com <abhiramKrishna.8921@gmail.com>
2021-11-15 08:56:24 -0800

    straight forward approach

---
+++ Prob_one.js
@@ -0,0 +1,32 @@
+/*
+Given an array of integer, return the indices
+of the 2 numbers that add upto a given
+target.
+*/
+
+//Ask these questions to interviewer => Constraints
+
+//Question1:- Are all the number positive or can ther be any negative numbers?
+//Question2:- Are there duplicate numbers in the array?
+//Question3:- Will there always be a solution availiable?
+//Question4:- What do we return if there are no solutions?
+//Question5:- Can there be multiple pairs that add upto this target number?
+
+const array = [1, 3, 7, 9, 2];
+
+const target = 11;
+
+function findPositionTarget(array, target) {
+  for (var i = 0; i < array.length; i++) {
+    p = i;
+
+    var numberToFind = target - array[p];
+    for (var j = p + 1; j < array.length; j++) {
+      if (array[j] == numberToFind) {
+        return [p, j];
+      }
+    }
+  }
+}
+
+console.log(findPositionTarget(array, target));
